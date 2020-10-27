98f29f4f76af2185eaf3e1adcf79062a8959f0c1
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-27 19:39:09 -0400

    Tested depsoit method.

--- BankAccountTester.java
+++ BankAccountTester.java
@@ -3,5 +3,9 @@ public static void main(String[]args){
   BankAccount b1 = new BankAccount(123456, "hello");
   System.out.println(b1.getBalance());
   System.out.println(b1.getAccountID());
+  System.out.println(b1.deposit(10.75));
+  System.out.println(b1.getBalance());
+  System.out.println(b1.deposit(-5.50));
+  System.out.println(b1.getBalance());
   }
 }
