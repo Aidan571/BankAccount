ce670a4f614525dc5a0b54cb7153e8f9a36341e9
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-27 19:27:35 -0400

    Created constructor for BankAccount.

--- BankAccount.java
+++ BankAccount.java
@@ -3,3 +3,11 @@ public class BankAccount{
   private double balance;
   private int accountID;
   private String password;
+  //constructor
+  public BankAccount(int aID, String p){
+  balance = 0;
+  accountID = aID;
+  password = p;
+  }
+
+}
