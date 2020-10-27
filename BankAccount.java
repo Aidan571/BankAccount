1472755bc180ac722f283ebd914814925f922aad
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-27 19:38:38 -0400

    Created deposit method.

--- BankAccount.java
+++ BankAccount.java
@@ -21,4 +21,14 @@ public class BankAccount{
   public void setPassword(String newPass){
   password = newPass;
   }
+
+  public boolean deposit(double amount){
+    if(amount < 0){
+      return false;
+    }
+    else
+    balance += amount;
+    return true;
+  }
+
 }
