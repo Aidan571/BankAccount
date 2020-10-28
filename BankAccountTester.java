0eb06bb323c7522114acbdad9fa27c3f79f3f3cd
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-27 20:00:28 -0400

    Tested toString method.

--- BankAccountTester.java
+++ BankAccountTester.java
@@ -13,5 +13,6 @@ public static void main(String[]args){
   System.out.println(b1.getBalance());
   System.out.println(b1.withdraw(-4.25));
   System.out.println(b1.getBalance());
+  System.out.println(b1.toString());
   }
 }
